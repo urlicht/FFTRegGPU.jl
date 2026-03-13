23c86ebe0d8ff6c1415756541a6a6585c871f454
urlicht <urlicht@users.noreply.github.com>
2026-03-13 21:54:25 +0000

    Revert "Add CUDA map-reduce argmax to avoid typemin error on complex types"
    This reverts commit 0c858a3bb78902e79e730a8d1a02ead94d6758fe.

--- ext/FFTRegGPUCUDAExt.jl
+++ ext/FFTRegGPUCUDAExt.jl
@@ -15,43 +15,6 @@ import FFTRegGPU: _fft, _ifft, _ifft!, _fftshift, _ifftshift, _scalar_at
 import FFTRegGPU: _plan_fft_inplace, _fft_inplace!
 import FFTRegGPU: _findmax_abs2_loc
 
-struct _EachIndex{T,N,IS} <: AbstractArray{T,N}
-    dims::NTuple{N,Int}
-    indices::IS
-end
-_EachIndex(A::AbstractArray) =
-    _EachIndex{typeof(firstindex(A)), ndims(A), typeof(eachindex(A))}(size(A), eachindex(A))
-Base.size(ei::_EachIndex) = ei.dims
-Base.getindex(ei::_EachIndex, i::Int) = ei.indices[i]
-Base.IndexStyle(::Type{<:_EachIndex}) = Base.IndexLinear()
-
-function _findmax_abs2_loc_cuda(inp::AbstractArray{<:Complex})
-    isempty(inp) && throw(ArgumentError("input must be non-empty"))
-    indices = _EachIndex(inp)
-    dummy_index = firstindex(inp)
-    Treal = float(real(eltype(inp)))
-
-    function reduction(t1, t2)
-        (x, i), (y, j) = t1, t2
-        if isless(x, y)
-            return t2
-        elseif isequal(x, y)
-            return (x, min(i, j))
-        else
-            return t1
-        end
-    end
-
-    res = mapreduce(
-        (x, i) -> (abs2(x), i),
-        reduction,
-        inp,
-        indices;
-        init=(typemin(Treal), dummy_index),
-    )
-    ndims(inp) == 1 ? res[2] : CartesianIndices(inp)[res[2]]
-end
-
 _fft(inp::CUDA.CuArray) = CUDA.CUFFT.fft(inp)
 _ifft(inp::CUDA.CuArray) = CUDA.CUFFT.ifft(inp)
 _ifft!(inp::CUDA.CuArray{<:Complex}) = CUDA.CUFFT.ifft!(inp)
@@ -62,8 +25,7 @@ _fft_inplace!(inp::CUDA.CuArray{<:Complex}, plan) = (plan * inp)
 _fftshift(inp::CUDA.CuArray) = CUDA.CUFFT.fftshift(inp)
 _ifftshift(inp::CUDA.CuArray) = CUDA.CUFFT.ifftshift(inp)
 _scalar_at(inp::CUDA.CuArray, idx) = CUDA.@allowscalar inp[idx]
-_findmax_abs2_loc(inp::CUDA.CuArray{<:Complex}, ::Union{Nothing,AbstractArray{<:Real}}=nothing) =
-    _findmax_abs2_loc_cuda(inp)
+_findmax_abs2_loc(inp::CUDA.CuArray{<:Complex}, ::Union{Nothing,AbstractArray{<:Real}}=nothing) = findmax(abs2, inp)[2]
 
 _fft(inp::SubArray{<:Any,<:Any,<:CUDA.CuArray}) = CUDA.CUFFT.fft(inp)
 _ifft(inp::SubArray{<:Any,<:Any,<:CUDA.CuArray}) = CUDA.CUFFT.ifft(inp)
@@ -73,6 +35,6 @@ _scalar_at(inp::SubArray{<:Any,<:Any,<:CUDA.CuArray}, idx) = CUDA.@allowscalar i
 _findmax_abs2_loc(
     inp::SubArray{<:Any,<:Any,<:CUDA.CuArray},
     ::Union{Nothing,AbstractArray{<:Real}}=nothing,
-) = _findmax_abs2_loc_cuda(inp)
+) = findmax(abs2, inp)[2]
 
 end
