11d205cf987b3b1b3b559444e77ea35b079fc740
urlicht <urlicht@users.noreply.github.com>
2026-03-13 10:04:31 +0000

    Refactor core registration code to be backend agnostic

--- src/FFTRegGPU.jl
+++ src/FFTRegGPU.jl
@@ -1,11 +1,22 @@
 module FFTRegGPU
-using CUDA, AbstractFFTs
+using AbstractFFTs
 
 include("dftreg_translate.jl")
 
-export dftreg_gpu!,
-	subpix_shift_gpu!,
-	dftreg_resample_gpu!,
-	reg_stack_translate!
+export dftreg!,
+    dftreg_subpix!,
+    subpix_shift!,
+    dftreg_resample!,
+    reg_stack_translate!,
+    dftreg_gpu!,
+    dftreg_subpix_gpu!,
+    subpix_shift_gpu!,
+    dftreg_resample_gpu!
+
+# Backward-compatible wrappers around the backend-agnostic API.
+dftreg_gpu!(args...) = dftreg!(args...)
+dftreg_subpix_gpu!(args...) = dftreg_subpix!(args...)
+subpix_shift_gpu!(args...) = subpix_shift!(args...)
+dftreg_resample_gpu!(args...) = dftreg_resample!(args...)
 
 end # module
--- src/dftreg_translate.jl
+++ src/dftreg_translate.jl
@@ -1,64 +1,89 @@
-function dftups(inp::AbstractArray{T,N},no,usfac::Int=1,offset=zeros(N)) where {T,N}
+_fft(inp::AbstractArray) = fft(inp)
+_ifft(inp::AbstractArray) = ifft(inp)
+_fftshift(inp::AbstractArray) = fftshift(inp)
+_ifftshift(inp::AbstractArray) = ifftshift(inp)
+
+function dftups(inp::AbstractArray{T,N}, no, usfac::Int=1, offset=zeros(N)) where {T,N}
     sz = [size(inp)...]
     permV = 1:N
     for i in permV
-        inp = permutedims(inp,[i;deleteat!(collect(permV),i)])
-        kern = exp.((-1im*2*pi/(sz[i]*usfac))*((0:(no-1)).-offset[i])*transpose(ifftshift(0:(sz[i]-1)).-floor(sz[i]/2)))
+        inp = permutedims(inp, [i; deleteat!(collect(permV), i)])
+        kern = exp.(
+            (-1im * 2 * pi / (sz[i] * usfac)) *
+            ((0:(no - 1)) .- offset[i]) *
+            transpose(ifftshift(0:(sz[i] - 1)) .- floor(sz[i] / 2)),
+        )
         d = size(inp)[2:N]
         inp = kern * reshape(inp, Val(2))
-        inp = reshape(inp,(no,d...))
+        inp = reshape(inp, (no, d...))
     end
-    permutedims(inp,collect(ndims(inp):-1:1))
+    permutedims(inp, collect(ndims(inp):-1:1))
 end
 
-function dftreg_gpu!(img1_f_g::CuArray{Complex{Float32},2}, img2_f_g::CuArray{Complex{Float32},2},
-    CC_g::CuArray{Complex{Float32},2})
-    L = length(img1_f_g)
-    CC_g .= CUFFT.ifft(img1_f_g .* CUDA.conj(img2_f_g))
-    loc = argmax(abs.(CC_g))
-    CCmax = Array(CC_g)[loc]
-    rfzero = sum(abs2, img1_f_g) / L
-    rgzero = sum(abs2, img2_f_g) / L
+function dftreg!(
+    img1_f::AbstractMatrix{<:Complex},
+    img2_f::AbstractMatrix{<:Complex},
+    CC::AbstractMatrix{<:Complex},
+)
+    size(img1_f) == size(img2_f) || throw(DimensionMismatch("img1_f and img2_f must have the same size"))
+    size(img1_f) == size(CC) || throw(DimensionMismatch("CC must have the same size as img1_f"))
+
+    L = length(img1_f)
+    CC .= _ifft(img1_f .* conj.(img2_f))
+    loc = argmax(abs.(CC))
+    CCmax = Array(CC)[loc]
+    rfzero = sum(abs2, img1_f) / L
+    rgzero = sum(abs2, img2_f) / L
     error = abs(1 - CCmax * conj(CCmax) / (rgzero * rfzero))
-    diffphase = atan(imag(CCmax),real(CCmax))
+    diffphase = atan(imag(CCmax), real(CCmax))
 
-    indi = size(img1_f_g)
-    ind2 = tuple([div(x,2) for x in indi]...)
+    indi = size(img1_f)
+    ind2 = tuple([div(x, 2) for x in indi]...)
+    locI = Tuple(loc)
+    shift = zeros(Float64, length(locI))
 
-    locI = [Tuple(loc)...]
-
-    shift = zeros(size(locI))
-    for i = eachindex(locI)
-        if locI[i]>ind2[i]
-            shift[i]=locI[i]-indi[i]-1
-        else shift[i]=locI[i]-1
+    for i in eachindex(locI)
+        if locI[i] > ind2[i]
+            shift[i] = locI[i] - indi[i] - 1
+        else
+            shift[i] = locI[i] - 1
         end
     end
-    
+
     error, shift, diffphase
 end
 
-function dftreg_subpix_gpu!(img1_f_g::CuArray{Complex{Float32},2}, img2_f_g::CuArray{Complex{Float32},2},
-    CC2x_g::CuArray{Complex{Float32},2}, up_fac::Int=10)
-    ## initial estimate by 2x upsample
-    # embed 2x fft
-    dim_input = collect(size(img1_f_g))
-    ranges = [(x+1-div(x,2)):(x+1+div(x-1,2)) for x in dim_input]
-    CC2x_g .= 0
-    CC2x_g[ranges...] .= CUFFT.fftshift(img1_f_g) .* CUFFT.conj(CUFFT.fftshift(img2_f_g))
+function dftreg_subpix!(
+    img1_f::AbstractMatrix{<:Complex},
+    img2_f::AbstractMatrix{<:Complex},
+    CC2x::AbstractMatrix{<:Complex},
+    up_fac::Int=10,
+)
+    size(img1_f) == size(img2_f) || throw(DimensionMismatch("img1_f and img2_f must have the same size"))
+    up_fac > 0 || throw(ArgumentError("up_fac must be positive"))
+
+    # initial estimate by 2x upsample
+    dim_input = collect(size(img1_f))
+    ranges = [(x + 1 - div(x, 2)):(x + 1 + div(x - 1, 2)) for x in dim_input]
+    expected_cc_size = tuple((2 .* dim_input)...)
+    size(CC2x) == expected_cc_size ||
+        throw(DimensionMismatch("CC2x must have size $(expected_cc_size), got $(size(CC2x))"))
+
+    CC2x .= 0
+    CC2x[ranges...] .= _fftshift(img1_f) .* conj.(_fftshift(img2_f))
 
     # compute cross-correlation and locate the peak
-    CC2x_g = CUFFT.ifft(CUFFT.ifftshift(CC2x_g))
-    loc = argmax(abs.(CC2x_g))
+    CC2x_corr = _ifft(_ifftshift(CC2x))
+    loc = argmax(abs.(CC2x_corr))
 
-    indi = size(CC2x_g)
+    indi = size(CC2x_corr)
     locI = collect(Tuple(loc))
-    CC2x_max = Array(CC2x_g)[loc]
+    CC2x_max = Array(CC2x_corr)[loc]
 
     # obtain shift in original pixel grid
     ind2 = indi ./ 2
-    shift = zeros(size(locI))
-    for i = eachindex(locI)
+    shift = zeros(Float64, length(locI))
+    for i in eachindex(locI)
         if locI[i] > ind2[i]
             shift[i] = locI[i] - indi[i] - 1
         else
@@ -67,88 +92,103 @@ function dftreg_subpix_gpu!(img1_f_g::CuArray{Complex{Float32},2}, img2_f_g::CuA
     end
     shift = shift / 2
 
-    ## refine subpixel estimation
+    # refine subpixel estimation
     if up_fac > 2
-        # refine the estimate with matrix multiply DFT
         shift = round.(Int, shift * up_fac) / up_fac # initial shift estimate
         dft_shift = ceil(up_fac * 1.5) / 2 # center of output at dft_shift + 1
 
-        # mat multiplies dft around the current shift estimate
-        CC_refine = dftups(Array(img2_f_g .* CUFFT.conj(img1_f_g)), ceil(Int, up_fac * 1.5),
-            up_fac, dft_shift .- shift * up_fac) / (prod(ind2) * up_fac ^ 2)
+        CC_refine = dftups(
+            Array(img2_f .* conj.(img1_f)),
+            ceil(Int, up_fac * 1.5),
+            up_fac,
+            dft_shift .- shift .* up_fac,
+        ) / (prod(ind2) * up_fac ^ 2)
 
-        # locate max and map back to the original grid
         loc = argmax(abs.(CC_refine))
         locI = Tuple(loc)
         CC_refine_max = CC_refine[loc]
         locI = locI .- dft_shift .- 1
         shift = shift .+ locI ./ up_fac
-        
-        img1_00 = dftups(Array(img1_f_g .* CUFFT.conj(img1_f_g)), 1, up_fac)[1] / (prod(ind2) * up_fac ^ 2)
-        img2_00 = dftups(Array(img2_f_g .* CUFFT.conj(img2_f_g)), 1, up_fac)[1] / (prod(ind2) * up_fac ^ 2)
+
+        img1_00 = dftups(Array(img1_f .* conj.(img1_f)), 1, up_fac)[1] / (prod(ind2) * up_fac ^ 2)
+        img2_00 = dftups(Array(img2_f .* conj.(img2_f)), 1, up_fac)[1] / (prod(ind2) * up_fac ^ 2)
         CC_max = CC_refine_max
     else
-        img1_00 = sum(img1_f_g .* CUFFT.conj(img1_f_g)) / prod(indi)
-        img2_00 = sum(img2_f_g .* CUFFT.conj(img2_f_g)) / prod(indi)
+        img1_00 = sum(img1_f .* conj.(img1_f)) / prod(indi)
+        img2_00 = sum(img2_f .* conj.(img2_f)) / prod(indi)
         CC_max = CC2x_max
     end
-    
+
     error = 1 - CC_max * conj(CC_max) / (img1_00 * img2_00)
-    error = sqrt(abs.(error))
+    error = sqrt(abs(error))
     diffphase = atan(imag(CC_max), real(CC_max))
-    
+
     error, shift, diffphase
 end
 
-function subpix_shift_gpu!(img_f_g::CuArray{Complex{Float32},2}, N_g::CuArray{Float32,2}, shift, diffphase)
-    sz = [size(img_f_g)...]
-    N_ = Float32(0)
-    for i = eachindex(sz)
-        shifti = ifftshift((-div(sz[i],2)):(ceil(Integer,sz[i]/2)-1))*shift[i]/sz[i]
-        resh = (repeat([1],inner=[i-1])...,length(shifti))
-        N_ = N_ .- Float32.(reshape(shifti,resh))
+function subpix_shift!(
+    img_f::AbstractMatrix{<:Complex},
+    N::AbstractMatrix{<:Real},
+    shift,
+    diffphase,
+)
+    size(img_f) == size(N) || throw(DimensionMismatch("N must have the same size as img_f"))
+
+    sz = [size(img_f)...]
+    T = float(real(eltype(img_f)))
+    N_ = zero(T)
+
+    for i in eachindex(sz)
+        shifti = ifftshift((-div(sz[i], 2)):(ceil(Int, sz[i] / 2) - 1)) * shift[i] / sz[i]
+        resh = (ntuple(_ -> 1, i - 1)..., length(shifti))
+        N_ = N_ .- T.(reshape(shifti, resh))
     end
-    
-    copyto!(N_g, N_)
-    exp(1im * diffphase) .* (img_f_g .* exp.(Complex{Float32}(2im * pi) * N_g))
+
+    copyto!(N, N_)
+    phase = cis(T(diffphase))
+    twopi_im = complex(zero(T), T(2 * pi))
+    phase .* (img_f .* exp.(twopi_im .* N))
 end
 
-function dftreg_resample_gpu!(img_f_g::CuArray{Complex{Float32}, 2}, N_g::CuArray{Float32,2}, shift, diffphase)
-    real(CUFFT.ifft(subpix_shift_gpu!(img_f_g, N_g, shift, diffphase)))
+function dftreg_resample!(
+    img_f::AbstractMatrix{<:Complex},
+    N::AbstractMatrix{<:Real},
+    shift,
+    diffphase,
+)
+    real(_ifft(subpix_shift!(img_f, N, shift, diffphase)))
 end
 
-function reg_stack_translate!(img_stack_reg_g::CuArray{Float32,3}, img1_f_g::CuArray{Complex{Float32},2},
-        img2_f_g::CuArray{Complex{Float32},2}, CC2x_g::CuArray{Complex{Float32},2},
-        N_g::CuArray{Float32,2}; reg_param::Dict)
-    size_x, size_y, size_z = size(img_stack_reg_g)
-    # reset arrays
-    CC2x_g .= 0
-    N_g .= 0
-    
-    # register
+function reg_stack_translate!(
+    img_stack_reg::AbstractArray{<:Real,3},
+    img1_f::AbstractMatrix{<:Complex},
+    img2_f::AbstractMatrix{<:Complex},
+    CC2x::AbstractMatrix{<:Complex},
+    N::AbstractMatrix{<:Real};
+    reg_param::AbstractDict{<:Integer}=Dict{Int,Any}(),
+)
+    _, _, size_z = size(img_stack_reg)
+    CC2x .= zero(eltype(CC2x))
+    N .= zero(eltype(N))
+
     for z = 2:size_z
         z1, z2 = z - 1, z
-        # copy data and fft
-        img1_g = view(img_stack_reg_g, :,:,z1)
-        img2_g = view(img_stack_reg_g, :,:,z2)
-        img1_f_g .= CUFFT.fft(img1_g)
-        img2_f_g .= CUFFT.fft(img2_g)
+        img1 = view(img_stack_reg, :, :, z1)
+        img2 = view(img_stack_reg, :, :, z2)
+        img1_f .= _fft(img1)
+        img2_f .= _fft(img2)
 
-        # register
         if !haskey(reg_param, z)
-            error, shift, diffphase = dftreg_subpix_gpu!(img1_f_g, img2_f_g, CC2x_g)
+            error, shift, diffphase = dftreg_subpix!(img1_f, img2_f, CC2x)
             reg_param[z] = (error, shift, diffphase)
         else
             error, shift, diffphase = reg_param[z]
         end
-        
-        # resample
-        img_stack_reg_g[:,:,z] .= dftreg_resample_gpu!(img2_f_g, N_g, shift, diffphase)
-        
-        # reset arrays
-        CC2x_g .= 0
-        N_g .= 0
+
+        img_stack_reg[:, :, z] .= dftreg_resample!(img2_f, N, shift, diffphase)
+        CC2x .= zero(eltype(CC2x))
+        N .= zero(eltype(N))
     end
-    
+
     nothing
 end
